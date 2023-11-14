383b0fe9c360a7dcab6dc4acf957e4d254fd81a0
netsmg <150570330+netsmg@users.noreply.github.com>
2023-11-14 09:34:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Paste your Firebase config object here
+const firebaseConfig = {
+  apiKey: "AIzaSyBzzaSTr42firr3fx5YcdXhDzB4iBLVBOM",
+  authDomain: "contact-database-9c47b.firebaseapp.com",
+  databaseURL: "https://contact-database-9c47b-default-rtdb.firebaseio.com",
+  projectId: "contact-database-9c47b",
+  storageBucket: "contact-database-9c47b.appspot.com",
+  messagingSenderId: "118822537955",
+  appId: "1:118822537955:web:3ef795baeadbe37b7293d6",
+  measurementId: "G-0WY0798WFC"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function handleFormSubmit() {
   const topicID = document.getElementById('topicID').value;
   const title = document.getElementById('title').value;
@@ -13,7 +28,9 @@ function handleFormSubmit() {
   };
 
   // Log the quiz object (you can replace this with your Firebase logic)
-  console.log('New Quiz:', quiz);
+  // For example, to push data to Firebase:
+  const quizzesRef = firebase.database().ref('quizzes');
+  quizzesRef.push(quiz);
 
   // Clear form fields after submission
   document.getElementById('topicID').value = '';
